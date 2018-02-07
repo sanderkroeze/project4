7880e70cb759acaeb2749b3c728b6f0bf2ee9906
sanderkroeze <35294605+sanderkroeze@users.noreply.github.com>
2018-02-07 09:55:33 +0100

    De nav balk is aangepast en wat javascript
    De nav balk is aangepast en wat javascript is toegevoegd aan HTML

--- Informatie over de cloud/cloud.js
+++ Informatie over de cloud/cloud.js
@@ -26,3 +26,45 @@ $(".whole").on("click","a",function(){
 
 });
 
+/* When the user clicks on the button, 
+toggle between hiding and showing the dropdown content */
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown if the user clicks outside of it
+window.onclick = function myFunction(event) {
+  if (!event.target.matches('.dropbtn')) {
+
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
+
+
+/* When the user clicks on the button, 
+toggle between hiding and showing the dropdown content */
+function myFunction1() {
+    document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown if the user clicks outside of it
+window.onclick = function myFunction1(event) {
+  if (!event.target.matches('.dropbtn')) {
+
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
---
+++ Social media/test.js
@@ -0,0 +1,5 @@
+function test(){
+	alert("U verlaat nu de pagina");
+}
+
+
